215b56e98aeb0fab2dc101d940a38af30d2c57c9
AsadAyyubKL <muhammadsaudfarooque@gmail.com>
2024-07-26 16:52:24 +0500

    Created a signup and login template.

---
+++ includes/templates/signup.php
@@ -0,0 +1,5 @@
+<?php
+
+// Template Name: SignUp Template
+
+echo "SignUp";
--- includes/wpls_integration.php
+++ includes/wpls_integration.php
@@ -1,18 +1,22 @@
 <?php
 
-
-// Register the custom template
-function my_custom_template_plugin_register_template( $templates ) {
+// Register the Custom Template
+function register_custom_template($templates)
+{
     $templates['login.php'] = 'Login Template';
+    $templates['signup.php'] = 'SignUp Template';
     return $templates;
 }
-add_filter( 'theme_page_templates', 'my_custom_template_plugin_register_template' );
+add_filter('theme_page_templates', 'register_custom_template');
 
-// Load the custom template
-function my_custom_template_plugin_load_template( $template ) {
-    if ( get_page_template_slug() == 'login.php' ) {
-        $template = plugin_dir_path( __FILE__ ) . '/templates/login.php';
+// Load the Custom Template
+function load_custom_template($template)
+{
+    if (get_page_template_slug() == 'login.php') {
+        $template = plugin_dir_path(__FILE__) . '/templates/login.php';
+    } else if (get_page_template_slug() == 'signup.php') {
+        $template = plugin_dir_path(__FILE__) . '/templates/signup.php';
     }
     return $template;
 }
-add_filter( 'template_include', 'my_custom_template_plugin_load_template' );
+add_filter('template_include', 'load_custom_template');
